cb5878244892e43be563a5892c38e847f9c073a1
Yash Gadodia <pirsquare.yash@gmail.com>
2020-09-26 13:06:41 +0800

    bugfix salad_spree

--- python-template-master/codeitsuisse/routes/salad_spree.py
+++ python-template-master/codeitsuisse/routes/salad_spree.py
@@ -1,26 +1,26 @@
-data = {
-    "number_of_salads" : 2,
-    "salad_prices_street_map" : [["2", "3", "X", "2"], ["4", "X", "X", "4"], ["3", "2", "X", "X"], ["X", "X", "X", "5"]]
-}
+# data = {
+#     "number_of_salads" : 2,
+#     "salad_prices_street_map" : [["2", "3", "X", "2"], ["4", "X", "X", "4"], ["3", "2", "X", "X"], ["X", "X", "X", "5"]]
+# }
 
-# import logging
-# import json
+import logging
+import json
 
-# from flask import request, jsonify;
+from flask import request, jsonify;
 
-# from codeitsuisse import app;
+from codeitsuisse import app;
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
-# @app.route('/salad-spree', methods=['POST'])
-# def evaluate():
-#     data = request.get_json();
-#     logging.info("data sent for evaluation {}".format(data))
-#     street_array = data.get("input");
+@app.route('/salad-spree', methods=['POST'])
+def evaluate():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    street_array = data.get("input");
 
-#     result = count_cost(street_array)
+    result = count_cost(street_array)
 
-#     return result;
+    return result;
 
 
 def count_cost(data):
@@ -44,11 +44,11 @@ def count_cost(data):
                     break
 
     if len(all_cost) > 0:
-        return {"result" : min(all_cost)}
+        return json.dumps({"result" : min(all_cost)})
     else:
-        return {"result" : 0}
+        return json.dumps({"result" : 0})
         
-print(count_cost(data))
+# print(count_cost(data))
 
 
     
