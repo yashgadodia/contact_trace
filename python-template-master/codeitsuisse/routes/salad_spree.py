ef06979187ed3485ac6e25a4e85018f237e69395
Yash Gadodia <pirsquare.yash@gmail.com>
2020-09-26 12:48:10 +0800

    add saladspree solution

---
+++ python-template-master/codeitsuisse/routes/salad_spree.py
@@ -0,0 +1,54 @@
+# data = {
+#     "number_of_salads" : 2,
+#     "salad_prices_street_map" : [["2", "3", "X", "2"], ["4", "X", "X", "4"], ["3", "2", "X", "X"], ["X", "X", "X", "5"]]
+# }
+
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/salad-spree', methods=['POST'])
+def evaluate():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    street_array = data.get("input");
+
+    result = count_cost(street_array)
+
+    return result;
+
+
+
+def count_cost(street_array):
+
+    all_cost = []
+
+    for i in range(len(street_array)):
+        number_of_salads = 0
+        cost = 0
+        consecutive_salad = 0
+        for j in street_array[i]:
+            if j == 'X':
+                consecutive_salad = 0
+            else:
+                cost += int(j)
+                number_of_salads += 1
+                consecutive_salad += 1
+                if number_of_salads == data["number_of_salads"] and consecutive_salad == data["number_of_salads"]:
+                    all_cost.append(cost)
+                    break
+
+    if len(all_cost) > 0:
+        return min(all_cost)
+    else:
+        return 0
+        
+print(count_cost(street_array))
+
+
+    
