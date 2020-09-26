fa7b3dd4018afde42917ed694274b350abb76abd
Yash Gadodia <pirsquare.yash@gmail.com>
2020-09-26 12:22:55 +0800

    init revisit geometry

---
+++ python-template-master/codeitsuisse/routes/revisitgeometry.py
@@ -0,0 +1,22 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/revisitgeometry', methods=['POST'])
+def evaluate():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("input");
+    # result = inputValue * inputValue
+    result = ["hello world 123"]
+    logging.info("My result :{}".format(result))
+    # return json.dumps(result);
+    return result;
+
+
+
