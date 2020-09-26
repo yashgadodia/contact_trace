175d26719cb8b9b902dea9f69c4a781227d10846
Yash Gadodia <pirsquare.yash@gmail.com>
2020-09-26 16:19:30 +0800

    passaway

--- python-template-master/codeitsuisse/routes/salad_spree.py
+++ python-template-master/codeitsuisse/routes/salad_spree.py
@@ -20,8 +20,73 @@ def evaluate():
 
     result = count_cost(street_array)
 
-    return result;
+    return result
 
+# data = {
+#     "number_of_salads" : 3,
+#     "salad_prices_street_map" : [["X", "X", "2"], ["2", "3", "X"], ["X", "3", "2"]]
+# }
+
+def count_cost(data):
+
+    street_array = data["salad_prices_street_map"]
+    salads = data["number_of_salads"]
+
+    all_cost = []
+
+    for arr in street_array:
+        min_cost = []
+        for j in range(len(arr)):
+            cost = 0
+            salad_count = 0
+            combi = arr[j:j+salads]
+            for elem in combi:
+                if elem.isnumeric() and salad_count != salads:
+                    cost += int(elem)
+                    salad_count += 1
+                if salad_count == salads:
+                    min_cost.append(cost)
+        
+        if len(min_cost) == 0:
+            return 0
+
+        all_cost.append(min(min_cost))
+
+    return min(all_cost)
+            
+        # store_cost = []
+        # store = street_array[arr]
+        # print(store[arr:salads])
+        # for j in street_array[i]:
+
+    # all_cost = []
+
+    # for i in range(len(street_array)):
+    #     number_of_salads = 0
+    #     cost = 0
+    #     consecutive_salad = 0
+    #     min_store_cost = []
+    #     for j in street_array[i]:
+    #         if j == 'X':
+    #             consecutive_salad = 0
+    #         else:
+    #             cost += int(j)
+    #             number_of_salads += 1
+    #             consecutive_salad += 1
+    #             if number_of_salads == data["number_of_salads"] and consecutive_salad == data["number_of_salads"]:
+    #                 all_cost.append(cost)
+    #                 break
+
+    # if len(all_cost) > 0:
+    #     return min(all_cost)
+    # else:
+    #     return 0
+    
+
+# print(count_cost(data))
+
+
+    
 
             
 
